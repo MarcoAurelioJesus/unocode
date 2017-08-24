2e10c0b72b90e684129afb30c3138bad0740ac93
Marco Aurélio de Jesus <jesus.marcoaurelio@gmail.com>
2017-08-24 12:30:57 -0300

    Add files via upload

--- unocode.js
+++ unocode.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 14);
+/******/ 	return __webpack_require__(__webpack_require__.s = 13);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -86,24 +86,6 @@ exports.Util = Util;
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
-
-const loaderDiv = document.createElement('div');
-
-loaderDiv.innerHTML = `
-    <img class="PRECIFICACAO-loader-img" src="https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif">
-    Carregando...
-`;
-
-loaderDiv.classList.add('PRECIFICACAO-loader', 'visible');
-document.body.appendChild(loaderDiv);
-
-setTimeout(() => {
-    loaderDiv.classList.remove('visible')
-}, 3000);
-
-/***/ }),
-/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -155,20 +137,20 @@ exports.ElementHidderCommandWithIncrement = ElementHidderCommandWithIncrement;
 
 
 /***/ }),
-/* 3 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-var types_1 = __webpack_require__(13);
-var js_command_1 = __webpack_require__(12);
-var elem_removal_command_1 = __webpack_require__(10);
-var elem_hidder_command_1 = __webpack_require__(2);
-var elem_hidder_command_2 = __webpack_require__(2);
-var html_importer_command_1 = __webpack_require__(11);
-var css_importer_command_1 = __webpack_require__(9);
-var configuration = __webpack_require__(8);
+var types_1 = __webpack_require__(12);
+var js_command_1 = __webpack_require__(11);
+var elem_removal_command_1 = __webpack_require__(9);
+var elem_hidder_command_1 = __webpack_require__(1);
+var elem_hidder_command_2 = __webpack_require__(1);
+var html_importer_command_1 = __webpack_require__(10);
+var css_importer_command_1 = __webpack_require__(8);
+var configuration = __webpack_require__(7);
 var Engine = (function () {
     function Engine() {
     }
@@ -283,7 +265,7 @@ exports.Engine = Engine;
 
 
 /***/ }),
-/* 4 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = [
@@ -292,7 +274,7 @@ module.exports = [
 
                                /*_______Ocultar Colunas grid______*/
   {
-    enabled: __webpack_require__(15),
+    enabled: __webpack_require__(14),
     nextActions: [
       
        {
@@ -318,7 +300,7 @@ module.exports = [
               var precificacao_panel = document.querySelector("#precificacao_panel > tbody > tr:nth-child(1)");
               setTimeout(() => {
               precificacao_panel.style.display = "none";
-              }, 3000);
+            }, 10000);
             }
           },
         ]
@@ -354,17 +336,17 @@ module.exports = [
           {
             type: "HTMLImporter",
             path: "//head",
-            html: __webpack_require__(7)
+            html: __webpack_require__(6)
           },
           {
             type: "CSSImporter",
-            css: __webpack_require__(5)
+            css: __webpack_require__(4)
           }
         ]
       },
       /*______Clicar no Botão Fluxo PL______*/
 
-      {
+     /* {
         guard: {
           conditions: [
             {
@@ -386,18 +368,18 @@ module.exports = [
               );
               
               setTimeout(() => {
-                __webpack_require__(1);
+                //require('./files/onloader.js');
                 btnAprovarProposta.click();
                 console.log("cliquei no botão Aprova");
               }, 3000);
             }
           },
         ]
-      },
+      },*/
 
       /*_________________________________________________________________*/
 
-      {
+     /* {
         guard: {
           conditions: [
             {
@@ -412,11 +394,11 @@ module.exports = [
         },
         commands: [
         ]
-      },
+      },*/
 
       /*Click no botão fluxo de aprovação*/
 
-      {
+      /*{
         guard: {
           conditions: [
             {
@@ -438,7 +420,7 @@ module.exports = [
               );
               if (tramitarPropostaLivreBt != null) {
                 tramitarPropostaLivreBt.addEventListener("click", () => {
-                   __webpack_require__(1);
+                   //require('./files/onloader.js');
                   setTimeout(() => {
                     var btnAprovarProp = document.getElementById(
                       "propostaLivre_workflowPropostaLivreButton"
@@ -452,11 +434,11 @@ module.exports = [
           },
           
         ]
-      },
+      },*/
 
       /*__Aciona o Dialog fluxo de aprovação ao clicar e OK do Dialog Atenção __*/
 
-      {
+      /*{
         guard: {
           conditions: [
             {
@@ -484,7 +466,7 @@ module.exports = [
               ) {
                 tramitarPropostaLivreBtn[0].addEventListener("click", () => {
 
-                  __webpack_require__(1);
+                 // require('./files/onloader.js');
                   setTimeout(() => {
                     var btnAprovarPropost = document.getElementById(
                       "propostaLivre_workflowPropostaLivreButton"
@@ -497,7 +479,7 @@ module.exports = [
             }
           },
         ]
-      },
+      },*/
     ]
   },
   
@@ -505,10 +487,10 @@ module.exports = [
 
 
 /***/ }),
-/* 5 */
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(6)(undefined);
+exports = module.exports = __webpack_require__(5)(undefined);
 // imports
 
 
@@ -519,7 +501,7 @@ exports.push([module.i, "/*__CSS PRECIFICAÇÃO PROPOSTA LIVRE__*/\r\n\r\nbody {
 
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, exports) {
 
 /*
@@ -601,19 +583,19 @@ function toComment(sourceMap) {
 
 
 /***/ }),
-/* 7 */
+/* 6 */
 /***/ (function(module, exports) {
 
 module.exports = "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></meta>";
 
 /***/ }),
-/* 8 */
+/* 7 */
 /***/ (function(module, exports) {
 
 module.exports = {"delayPolling":"1000"}
 
 /***/ }),
-/* 9 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -637,7 +619,7 @@ exports.CSSImporterCommand = CSSImporterCommand;
 
 
 /***/ }),
-/* 10 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -661,7 +643,7 @@ exports.ElementRemovalCommand = ElementRemovalCommand;
 
 
 /***/ }),
-/* 11 */
+/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -687,7 +669,7 @@ exports.HTMLImporterCommand = HTMLImporterCommand;
 
 
 /***/ }),
-/* 12 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -709,7 +691,7 @@ exports.JSCommand = JSCommand;
 
 
 /***/ }),
-/* 13 */
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -727,23 +709,23 @@ var CommandType;
 
 
 /***/ }),
-/* 14 */
+/* 13 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-var engine_1 = __webpack_require__(3);
-var actionsList = __webpack_require__(4);
+var engine_1 = __webpack_require__(2);
+var actionsList = __webpack_require__(3);
 engine_1.Engine.runActions(actionsList);
 
 
 /***/ }),
-/* 15 */
+/* 14 */
 /***/ (function(module, exports) {
 
 module.exports = () => {
-    let check = true;
+    let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
 }
